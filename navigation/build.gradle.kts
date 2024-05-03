7b1bc1c6e008e00e9d226e24aafe949ccd8a12cf
FnGoose <empirx5530@gmail.com>
2024-05-03 23:22:20 +0800

    feat: update publish version.

--- navigation/build.gradle.kts
+++ navigation/build.gradle.kts
@@ -63,7 +63,7 @@ afterEvaluate {
                 artifact(tasks["generateSourcesJar"])
                 groupId = "little.goose"
                 artifactId = "viewnavigation"
-                version = "1.0.8"
+                version = "1.0.9"
             }
         }
     }
